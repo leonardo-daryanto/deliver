2e85e0a975b0503f810943e5138a4bce6cdae616
leonardo-daryanto <103314397+leonardo-daryanto@users.noreply.github.com>
2022-07-12 10:09:37 +0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
-basic.forever(function () {
-	
-})
+
+namespace array {
+    /**
+     *  Make a block do deliver an array e.g. food array delivered to list array
+     */
+    //% block
+    //% group="Advanced"
+    export function deliver(firstArray: any, secondArray: any, getValueFromSecondArray: number): void {
+        firstArray.unshift(secondArray[getValueFromSecondArray])
+    }
+}
